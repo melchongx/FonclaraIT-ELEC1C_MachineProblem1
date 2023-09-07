0363ce75e93a4ad36aff34391ff25809828c72be
melchongx <melfredfonclara@gmail.com>
2023-09-07 20:23:36 +0800

    Add project files.

---
+++ FonclaraIT-ELEC1C_MachineProblem1/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FonclaraIT_ELEC1C_MachineProblem1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
