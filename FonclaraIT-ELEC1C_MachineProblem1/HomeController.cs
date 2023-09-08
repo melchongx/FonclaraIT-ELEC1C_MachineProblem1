2d3087c4770b2c55c19170c2d45253d7ae0af5b6
melchongx <melfredfonclara@gmail.com>
2023-09-08 08:32:42 +0800

    finished entry

---
+++ FonclaraIT-ELEC1C_MachineProblem1/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FonclaraIT_ELEC1C_MachineProblem1
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
